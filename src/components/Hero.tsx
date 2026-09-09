9f0714ef7893e9b8292ed28d0a8a7e586c78e27f
Nikita Sinha <nikitaa.sinhaa@gmail.com>
2026-09-10 00:37:25 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,7 +28,7 @@ const Hero = () => {
             Nikita Sinha
           </h1>
           <h2 className="text-2xl md:text-3xl text-muted-foreground font-light animate-fade-in stagger-1">
-           Embedded  & FPGA Engineer
+           <b>Embedded  & FPGA Engineer </b> </h2>
           </h2>
           <h3 className="text-2xl md:text-3xl text-muted-foreground font-light animate-fade-in stagger-1">
            Firmware • FPGA • Real-Time Systems • Edge AI
