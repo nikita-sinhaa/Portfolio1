d6927ed948ecea1a410e48e2e4f166bc8ab4b1dc
Nikita Sinha <nikitaa.sinhaa@gmail.com>
2026-09-09 22:52:08 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="flex flex-col items-center justify-center space-y-4">
           <div className="flex items-center space-x-6">
             <a
-              href="https://drive.google.com/file/d/1H750Ikm7NE9JBSEgwqVa4rbt-N6aZkHt/view?usp=drive_link"
+              href="https://drive.google.com/file/d/15pkYIV9qqQK27bncmFZ_Z9hwZcAjLnUq/view?usp=drive_link"
               target="_blank"
               rel="noopener noreferrer"
               className="flex items-center space-x-2 text-muted-foreground hover:text-primary transition-colors"
