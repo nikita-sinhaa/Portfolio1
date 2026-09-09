5719485810db4c58a60839ec1111b63f1f18a41b
Nikita Sinha <nikitaa.sinhaa@gmail.com>
2026-09-10 01:36:41 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -23,10 +23,10 @@ const About = () => {
         <div className="text-center mb-16 animate-fade-in">
          <h2 class="text-4xl font-bold mb-4 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text inline-block"> BEYOND ENGINEERING <span class="text-white">WHO IS NIKITA? </span> </h2>
           <p className="text-xl text-muted-foreground max-w-3xl mx-auto animate-fade-in stagger-1">
-            Embedded Engineer, sensor enthusiast, and a proud real-time systems fanatic.
-Builder of firmware, FPGA logic, and systems that sense, compute, and respond.
-I turn hardware challenges into reliable solutions—and make code work as hard as the hardware it runs on.
-
+            Embedded Engineer, sensor enthusiast, and a proud real-time systems fanatic.</p>
+<p className="text-xl text-muted-foreground max-w-3xl mx-auto animate-fade-in stagger-1">Builder of firmware, FPGA logic, and systems that sense, compute, and respond.
+I turn hardware challenges into reliable solutions and make code work as hard as the hardware it runs on.</p>
+<p className="text-xl text-muted-foreground max-w-3xl mx-auto animate-fade-in stagger-1">
 Outside the lab, you’ll find me writing, exploring new places, cooking something spicy, or photographing the most random building around.
 
           </p>
